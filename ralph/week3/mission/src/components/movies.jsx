534d53567ba96c2269233ffd082c34a3adbf8726
정휘준 <jeonghwijun@jeonghwijun-ui-MacBookPro.local>
2024-10-11 01:00:45 +0900

    now-playing 구현

---
+++ ralph/week3/mission/src/components/movies.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import "./movie.css"
+const IMG_BASE_URL ="https://image.tmdb.org/t/p/w500/"
+
+export default function Movies ({key,movie}){
+
+  return (
+   <div className='movie-container'>
+    <div class='a'>
+      <img src={IMG_BASE_URL + movie.poster_path}></img>
+      </div>
+      <div className='movie-info'>
+         <h4>{movie.title}</h4>
+         <span>{movie.vote_average}</span>
+       </div>
+      </div> 
+  );
+}
+
