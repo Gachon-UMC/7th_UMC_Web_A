3d0a28f28fdc12ff902dd3423b3c7920e7585eec
정휘준 <jeonghwijun@jeonghwijun-ui-MacBookPro.local>
2024-10-14 16:52:32 +0900

    4주차 mission 1 완료

--- ralph/week4/mission/src/apis/axios-instance.js
+++ ralph/week4/mission/src/apis/axios-instance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios .create ({
   headers: {
     Authorization : `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
   },
-  baseURL: import.meta.env.VITE_TMDB_TOKEN,
+  baseURL: import.meta.env.VITE_MOVIE_API_URL,
 })  
 
 export {axiosInstance}
--- ralph/week4/mission/src/components/movies.jsx
+++ ralph/week4/mission/src/components/movies.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 // import "./movie.css"
 const IMG_BASE_URL ="https://image.tmdb.org/t/p/w500/"
 
-export default function Movies ({key,movie}){
+export default function Movies ({movie}){
 
   return (
    <div className='movie-container'>
-    <div class='a'>
+    <div className='a'>
       <img src={IMG_BASE_URL + movie.poster_path}></img>
       </div>
       <div className='movie-info'>
