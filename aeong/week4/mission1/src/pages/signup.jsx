dc46f4b8f0121be3c33d4b107d463728b3110615
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2024-10-16 04:23:40 +0900

    week4 mission1

---
+++ aeong/week4/mission1/src/pages/signup.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const SignupPage = () => {
+  return <Signup>회원가입 페이지</Signup>;
+};
+
+export default SignupPage;
+
+const Signup = styled.h1`
+  background-color: black;
+  color: white;
+  margin-left: 10px;
+`;
