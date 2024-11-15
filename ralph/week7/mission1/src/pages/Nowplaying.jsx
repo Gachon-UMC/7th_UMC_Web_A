872fe59106998d954987e133b2dbca9f00a70f88
정휘준 <jeonghwijun@jeonghwijun-ui-MacBookPro.local>
2024-11-15 15:28:02 +0900

    7주차 미션1 수정완료

--- ralph/week7/mission1/src/pages/Nowplaying.jsx
+++ ralph/week7/mission1/src/pages/Nowplaying.jsx
@@ -17,20 +17,19 @@ const Nowplaying = () => {
         isLoading,
         isError,
     } = useQuery({
-        queryKey: ["NowPlaying"],
-        queryFn: () => useGetMovies({ category: "now_playing", pageParam: 1 }),
-        cacheTime: 10000,
+        queryKey: ["NowPlaying"], // 이 코드는 Query Client 라는 중앙 캐시 저장소 에서 key 값이 "NowPlaying" 인 값을 가져오는 코드
+        queryFn: () => useGetMovies({ category: "now_playing", pageParam: 1 }), // 이건 fresh 상태 이후에 stale 상태에서 Query Client 에 key 값이 "NowPlaying" 인 값이 없을 때 해당 함수를 실행해서 서버로 부터 데이터를 가지고 오는 코드로 가지고 온 데이터는 key 값이 NowPlaying 으로 캐시에 저장된다.
         staleTime: 10000,
     });
     console.log(movies);
 
     // 구조분해할당으로 안받으면
-    // const queryresults = useQuery(["NowPlaying"], async () => {
+    // const queryresults = useQuery({["NowPlaying"], async () => {
     //     const response = await axiosInstance.get(
     //         `/movie/now_playing?language=ko&page=1&region=KR`
     //     );
     //     return response.data.results;
-    // });
+    // }});
     // const data= queryresutls.data;
     //const isLoading = queryresults.isLoading;
     // 이런식으로 받아와야함
