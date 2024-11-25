10927ae7edc650eabb414808c63edb9ed82615d4
정휘준 <jeonghwijun@jeonghwijun-ui-MacBookPro.local>
2024-11-25 16:25:33 +0900

    delete forders

--- ralph/week9/mission1/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import playListReducer from "./cartSlice";
-const store = configureStore({
-    reducer: { playList: playListReducer },
-});
-export default store;
--- ralph/week9/practice/src/components/errorpage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Errorpage = () => {
-    return <div>상세페이지 실패</div>;
-};
-
-export default Errorpage;
