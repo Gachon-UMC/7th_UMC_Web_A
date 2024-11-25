c5a4bad79dfea08f9e9feac5887dc7ff8ff07538
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2024-11-25 19:10:20 +0900

    delete folders

--- aeong/week2/mission1/vite-project/src/Button.jsx
+++
@@ -1,8 +0,0 @@
-const Button = (props) => {
-  return (
-    <button className="add-button" type="submit" onClick={props.onClick}>
-      {props.text}
-    </button>
-  );
-};
-export default Button;
--- aeong/week8/mission3/src/hooks/queries/useGetMovieDetail.js
+++
@@ -1,9 +0,0 @@
-import { axiosInstance } from "../../apis/axios-instance";
-
-const useGetMovieDetail = async (movieId) => {
-  const { data } = await axiosInstance.get(
-    `/movie/${movieId}/credits?language=ko-KR&page=1`
-  );
-  return data;
-};
-export default useGetMovieDetail;
